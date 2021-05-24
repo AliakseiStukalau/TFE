9d7d824168dbab7a08ca0e9274fd908e939abb7e
Aliaksei Stukalau <aliaksei.stukalau@globant.com>
2021-05-25 02:09:53 +0300

    Add Child class

---
+++ Source/TFE/PlayerChild.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AFireplace.h"
+#include "PlayerChild.generated.h"
+
+UCLASS()
+class TFE_API APlayerChild : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlayerChild();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* ChildMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float HitPoints;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AAFireplace* Fireplace;
+};
